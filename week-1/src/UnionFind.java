8dfd06337148ccbff88fe392a189a5f8bc24601d
Sumit Jain <sumeetsuniljain@gmail.com>
2020-09-20 19:12:11 +0530

    Weighted Quick Union

--- week-1/src/UnionFind.java
+++ week-1/src/UnionFind.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class UnionFind {
     public static void main(String [] args) {
-        System.out.println("Hello, world!");
+        QuickFindUF a = new QuickFindUF(10);
+
+        a.union(5,6);
+        a.union(5,0);
+        a.union(1,2);
+        a.union(3,4);
+        a.union(3,4);
+        a.union(3,8);
+        a.union(8,9);
+
+        System.out.println(a.connected(4,9));
     }
 }
